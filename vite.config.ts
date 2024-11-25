331f86ab228b72ccd1cc6429eabe74581279823a
siyabongamthethwa70 <siyabongamthethwa70@gmail.com>
2024-11-25 15:11:17 +0200

    Added basic ssl plugin to Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import type { ServerOptions } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  server: {
+    https: {} as ServerOptions['https'], // This is the correct type-safe way
+    port: 3000,
   },
 });
